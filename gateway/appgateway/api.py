907a5532809eb1eb0f1d369bbb21075fcd6d687d
Matheus Lagos <60862073+Matheuslagos@users.noreply.github.com>
2024-09-11 22:51:40 -0300

    adicionando outra api

--- gateway/appgateway/api.py
+++ gateway/appgateway/api.py
@@ -3,12 +3,20 @@ import requests
 
 api = NinjaAPI(urls_namespace='appgetway')
 
-# Exemplo de endpoint no API Gateway que orquestra as chamadas para outra API
-@api.get("/")
-def get_getway(request, a: int, b: int):
-    # URL da outra API (que está sendo orquestrada)
+# Orquestra chamada para o endpoint "add" da API "appendpoint"
+@api.get("/add/")
+def get_add(request, a: int, b: int):
     api_url = f"http://127.0.0.1:8000/api/endpoint/add?a={a}&b={b}"
     response = requests.get(api_url)
     
-    # Retorna a resposta da outra API
+    # Retorna a resposta da API "appendpoint"
+    return response.json()
+
+# Orquestra chamada para o endpoint "sub" da API "appendpoint"
+@api.get("/sub/")
+def get_sub(request, a: int, b: int):
+    api_url = f"http://127.0.0.1:8000/api/endpoint/sub?a={a}&b={b}"
+    response = requests.get(api_url)
+    
+    # Retorna a resposta da API "appendpoint"
     return response.json()
