8bb239a5cf48bcfd4e9ec6394273874e7fc14dda
Matheus Lagos <60862073+Matheuslagos@users.noreply.github.com>
2024-09-13 17:06:23 +0000

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <router-link to="/chat">Go to Chat</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
