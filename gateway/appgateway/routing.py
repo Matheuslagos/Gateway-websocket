238f22d42d6e4465342ac82aac5250077ca16795
Matheus Lagos <60862073+Matheuslagos@users.noreply.github.com>
2024-09-15 16:34:31 -0300

    retirando routing

--- gateway/appgateway/routing.py
+++
@@ -1,6 +0,0 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from appwebsocket.routing import websocket_urlpatterns as ws_patterns
-
-application = ProtocolTypeRouter({
-    "websocket": URLRouter(ws_patterns)  # Roteia os WebSocket para o appwebsocket
-})
