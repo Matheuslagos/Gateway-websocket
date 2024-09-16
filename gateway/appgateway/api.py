276d77a27be50fe1ce1afe4922f17e57cab46f0f
Matheus Lagos <60862073+Matheuslagos@users.noreply.github.com>
2024-09-15 21:37:34 -0300

    ajeita

--- gateway/appgateway/api.py
+++ gateway/appgateway/api.py
@@ -3,22 +3,18 @@ import requests
 
 api = NinjaAPI(urls_namespace='appgetway')
 
-# Orquestra chamada para o endpoint "add" da API "appendpoint"
 @api.get("/add/")
 def get_add(request, a: int, b: int):
     api_url = f"http://127.0.0.1:8000/api/endpoint/add?a={a}&b={b}"
     response = requests.get(api_url)
     
-    # Retorna a resposta da API "appendpoint"
     return response.json()
 
-# Orquestra chamada para o endpoint "sub" da API "appendpoint"
 @api.get("/sub/")
 def get_sub(request, a: int, b: int):
     api_url = f"http://127.0.0.1:8000/api/endpoint/sub?a={a}&b={b}"
     response = requests.get(api_url)
     
-    # Retorna a resposta da API "appendpoint"
     return response.json()
 
 @api.get("/cep/")
@@ -26,5 +22,4 @@ def get_cep(request, cep:int):
     api_url = f"https://viacep.com.br/ws/{cep}/json/"
     response = requests.get(api_url)
     
-    # Retorna a resposta da API "appendpoint"
     return response.json()
